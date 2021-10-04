14e5fdffb26be5938470a802bf9a02629a1cefaa
Jhanvi <68721052+Jhanvi2001@users.noreply.github.com>
2021-10-04 12:23:34 +0530

    sentiment analysis

---
+++ dash_sentiment_textblob.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 11 10:08:07 2021
+
+@author: sjhan
+"""
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+import sqlite3
+from textblob import TextBlob
+from unidecode import unidecode
+import time
+
+
+
+conn = sqlite3.connect('final_textblob.db')
+c = conn.cursor()
+
+def create_table():
+    try:
+        c.execute("CREATE TABLE IF NOT EXISTS sentiment(unix REAL, tweet TEXT, sentiment REAL)")
+        c.execute("CREATE INDEX unix ON sentiment(unix)")
+        c.execute("CREATE INDEX tweet ON sentiment(tweet)")
+        c.execute("CREATE INDEX sentiment ON sentiment(sentiment)")
+        conn.commit()
+    except Exception as e:
+        print(str(e))
+create_table()
+
+#consumer key, consumer secret, access token, access secret.
+ckey="wwoXP6iLVHXI5za08zudK90xF"
+csecret="Aq6lajJ56O0neFsWJT4aR99XCOJ5vs14EmnrAhKjGEEfQ4kWCA"
+atoken="1433817013793607680-4uNjHfglzFDvm8xft5zqMom4GXKLux"
+asecret="ptVGkS3QnOxIu3bY3RrEU2FDuOLj3zRB0A3h2x1LEVvm3"
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        try:
+            data = json.loads(data)
+            tweet = unidecode(data['text'])
+            time_ms = data['timestamp_ms']
+            analysis=TextBlob(tweet)
+            
+            sentiment = analysis.sentiment.polarity
+            print(time_ms, tweet, sentiment)
+            c.execute("INSERT INTO sentiment (unix, tweet, sentiment) VALUES (?, ?, ?)",
+                  (time_ms, tweet, sentiment))
+            conn.commit()
+
+        except KeyError as e:
+            print(str(e))
+        return(True)
+
+    def on_error(self, status):
+        print(status)
+
+
+while True:
+
+    try:
+        auth = OAuthHandler(ckey, csecret)
+        auth.set_access_token(atoken, asecret)
+        twitterStream = Stream(auth, listener())
+        twitterStream.filter(track=["a","e","i","o","u"])
+    except Exception as e:
+        print(str(e))
+        time.sleep(5)
+
